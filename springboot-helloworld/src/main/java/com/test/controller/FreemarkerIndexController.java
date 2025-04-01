8088ad6d766a3111f13350cfaf44cd6b95e102c5
郑春皓 <>
2025-04-01 19:51:47 +0800

    no message

--- springboot-helloworld/src/main/java/com/test/controller/FreemarkerIndexController.java
+++ springboot-helloworld/src/main/java/com/test/controller/FreemarkerIndexController.java
@@ -14,7 +14,8 @@ public class FreemarkerIndexController {
     public String freemarkerIndex(Map<String, Object> result, HttpServletRequest request){
         System.out.println("Controller method invoked!"); // 确认是否打印
         result.put("name","test1111");
-        result.put("age","18");
+        result.put("sex","0");
+        result.put("age",18);
         ArrayList<String> userlist = new ArrayList<>();
         userlist.add("user1");
         userlist.add("user2");
--- springboot-helloworld/src/main/resources/templates/freemarkerIndex.ftl
+++ springboot-helloworld/src/main/resources/templates/freemarkerIndex.ftl
@@ -6,5 +6,21 @@
 </head>
 <body>
 ${name}
+
+<#if sex=='0'>
+    <p>男</p>
+<#elseif sex=='1'>
+    <p>女</p>
+<#else>
+    <p>未知</p>
+</#if>
+<#if age gt 18>
+<p>成年</p>
+<#else>
+    <p>未成年</p>
+</#if>
+<#list userlist as user>
+    <p>${user}</p>
+</#list>
 </body>
 </html>
